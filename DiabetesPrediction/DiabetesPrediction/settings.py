8bd51f939b9a66f4291b96b0dcb1b1c30612913c
Jephin <jephintj@gmail.com>
2024-03-20 22:39:06 +0530

    unwanted deletion

--- DiabetesPrediction/DiabetesPrediction/settings.py
+++ DiabetesPrediction/DiabetesPrediction/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7&kt*8=l-*nzcz2cyac+c83lydu)zyao)cr6gufg4k7*ge0-lj'
+SECRET_KEY = 'django-insecure-s^6u=bg3mhhkrz!tqyltk3*ect!s)t!if^_tw8qqu%+$j2$l3q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'DiabetesPrediction.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,8 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,'static'),
-)
-STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),'static')
