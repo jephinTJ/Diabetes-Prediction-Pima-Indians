13e47becd04a1e0dc9399d0b8bbbc638dcf92513
Jephin <jephintj@gmail.com>
2024-03-21 11:02:12 +0530

    index page coding completed and tested

--- DiabetesPrediction/DiabetesPrediction/settings.py
+++ DiabetesPrediction/DiabetesPrediction/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), 'static')
